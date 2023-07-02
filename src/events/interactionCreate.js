74ac40fe11a397f0186ceeda45c688e145a46cc5
battleless <62182223+battleless@users.noreply.github.com>
2023-07-02 04:22:33 -0400

    remove components

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -22,25 +22,6 @@ module.exports = {
                     ephemeral: true
                 });
             }
-        } else if (interaction.isButton() || interaction.isAnySelectMenu() || interaction.isModalSubmit()) {
-            const component = interaction.client.components.get(interaction.customId);
-
-            if (!component) return;
-                
-            try {
-                await component.run(interaction);
-            } catch (error) { 
-                console.error(error);
-
-                if (interaction.replied || interaction.deferred) {
-                    await interaction.message.reply({
-                        content: `${interaction.client.user.username} encountered an issue while executing this component.\n${error.name}: ${error.message}`
-                    });
-                } else await interaction.reply({
-                    content: `${interaction.client.user.username} encountered an issue while executing this component.\n${error.name}: ${error.message}`,
-                    ephemeral: true
-                });
-            }
         }
     }
 }
